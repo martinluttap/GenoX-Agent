58c0d0460ed4c79fdbe1170acd6da6cbf228cbc1
Martin L. Putra <martinluttap@uchicago.edu>
2024-09-26 15:52:49 +0000

    checkpoint

--- controller/controller.go
+++ controller/controller.go
@@ -35,7 +35,7 @@ func TickWriter(activeContainersCh <-chan []string, intervalMillisecond int, sto
 					ss := strings.Split(containerDir, "/")
 					cid := ss[len(ss)-1]
 					fmt.Println("Adjusting quota for ", cid)
-					adjustQuota(containerDir, elapsedTime, "numThreads")
+					adjustQuota(containerDir, elapsedTime, "constant")
 				}
 				// for _, containerDir := range controlVariableContainers {
 				// 	ss := strings.Split(containerDir, "/")
