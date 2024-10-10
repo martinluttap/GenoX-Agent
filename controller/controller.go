55828e5fcdaf40bed77f30c0c7586f2c4d6656aa
Martin L. Putra <martinluttap@uchicago.edu>
2024-10-10 16:25:32 +0000

    Fixed conflict

--- controller/controller.go
+++ controller/controller.go
@@ -109,11 +109,7 @@ func adjustQuota(containerDir string, elapsedTime float64, functionName string) 
 		oldPeriod := s.Text()
 		// f(x): constant
 		if functionName == "constant" {
-<<<<<<< HEAD
 			allocatedCores := int64(10)
-=======
-			allocatedCores := int64(158)
->>>>>>> d84d343ccfcf1b39c40086cde97ba3ede4a0ec5e
 			newPeriod = strconv.FormatInt(allocatedCores*10000, 10)
 			fmt.Printf("Test %d\n", allocatedCores)
 			// newPeriod = strconv.FormatInt(allocatedCores*100000, 10)
