72955dea54fad7d81d49875e7cc6be869db9a5e6
Martin L. Putra <martinluttap@uchicago.edu>
2024-11-10 23:42:57 +0000

    Checkpoint before turning off machines

--- experiments/driver/corr-by_policies.py
+++ experiments/driver/corr-by_policies.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 
             print(f'============ Timestamp:{datetime.datetime.now()},app={APP},policy={POLICY}, RUN={RUN}  ============')
 
-            LABEL = f"{RUN}-{POLICY}-{APP}"
+            LABEL = f"contention_2-{RUN}-{POLICY}-{APP}"
             OUT_LOG = f"{LABEL}.log"
 
             # Run prep
