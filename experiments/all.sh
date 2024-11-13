c2a58023b3e2f01e94a3504d2e8dcfb1ee84d396
Martin L. Putra <martinluttap@uchicago.edu>
2024-11-13 17:00:47 +0000

    Try separate elasticcontainer go module

--- experiments/all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POLICIES="autothrottle"
-APPS="trimmomatic"
-for POLICY in $POLICIES; do
-  for APP in $APPS; do
-    python3 driver/corr-by_policies.py --app ${APP} --policy ${POLICY}
-  done
-done
