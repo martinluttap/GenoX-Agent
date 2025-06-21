9a25b76661006dc62d033ea2721edb11707eaed1
Martin L. Putra <martinluttap@uchicago.edu>
2025-06-21 20:12:12 +0000

    Checkpoint showar working

--- controller/controller.go
+++ controller/controller.go
@@ -181,7 +181,7 @@ func adjustQuotaV2(containerDir string, elapsedTime float64, functionName string
 		} else if functionName == "noLimit" {
 			newPeriod = functions.NoLimit(containerDir)
 		} else if functionName == "staticN" {
-			newPeriod = functions.StaticN(8)
+			newPeriod = functions.StaticN(92)
 		}
 		if val, ok := allowedFunctions[functionName]; !ok {
 			fmt.Println("Function ", val, " not allowed!")
