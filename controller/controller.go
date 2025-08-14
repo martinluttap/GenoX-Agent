d8869794e9c951eb51a2fefa6df139913c1cd99f
Martin L. Putra <martinluttap@gmail.com>
2025-08-14 21:21:18 +0000

    checkpoint

--- controller/controller.go
+++ controller/controller.go
@@ -176,7 +176,7 @@ func adjustQuota(containerDir string, elapsedTime float64, functionName string) 
 		} else if functionName == "noLimit" {
 			newPeriod = functions.NoLimit(containerDir)
 		} else if functionName == "staticN" {
-			newPeriod = functions.StaticN(0)
+			newPeriod = functions.StaticN(16)
 		}
 		if val, ok := allowedFunctions[functionName]; !ok {
 			fmt.Println("Function ", val, " not allowed!")
